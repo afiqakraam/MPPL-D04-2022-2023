e5eef6d65182962a4cd19423de4c321d03ff27cb
Arraidh <ari.darmika@gmail.com>
2022-12-04 23:02:41 +0700

    CREATE ROUTES & INSERT DATA IN DATABASE

--- server/data/users.js
+++ server/data/users.js
@@ -1,16 +1,16 @@
-import bcrypt from "brcyptjs";
+import bcrypt from 'bcryptjs';
 
 const users = [
     {
         name: "Admin",
         email: "admin@example.com",
-        password: bcrypt.hasSync("123456", 10),
+        password: bcrypt.hashSync("123456", 10),
         isAdmin: true
     },
     {
         name: "user",
         email: "user@example.com",
-        password: bcrypt.hasSync("123456", 10),
+        password: bcrypt.hashSync("123456", 10),
     }
 ];
 
--- server/server.js
+++ server/server.js
@@ -1,24 +1,16 @@
 import express from "express";
-import products from "./data/Products.js";
 import dotenv from "dotenv";
 import connectDatabase from "./config/MongoDB.js";
+import ImportData from "./DataImport.js";
+import productRoute from "./Routes/ProductRoutes.js";
 
 dotenv.config();
 connectDatabase();
 const app = express();
 
-
-// Load product from server
-app.get("/api/products", (req, res) => {
-    res.json(products);
-});
-
-// Single product from server
-app.get("/api/products/:id", (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
-
+// API
+app.use("/api/import", ImportData);
+app.use("/api/products", productRoute);
 
 app.get("/", (req, res) => {
     res.send("API is Running....");
