41843a966bbd22e3abb6a33036ed1bb2f0c1693e
Arraidh <ari.darmika@gmail.com>
2022-12-05 14:51:14 +0700

    Add redux in single product

--- frontend/src/Redux/Reducers/ProductReducers.js
+++ frontend/src/Redux/Reducers/ProductReducers.js
@@ -1,5 +1,6 @@
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../Constants/ProductConstants";
+import { PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../Constants/ProductConstants";
 
+// PRODUCT LIST
 export const productListReducer = (state = { products: [] }, action) => {
     switch (action.type) {
         case PRODUCT_LIST_REQUEST:
@@ -11,4 +12,19 @@ export const productListReducer = (state = { products: [] }, action) => {
         default:
             return state;
     }
-}
+};
+
+// SINGLE PRODUCT
+export const productDetailsReducer = (state = { product: { reviews: [] } }, action) => {
+    switch (action.type) {
+        case PRODUCT_DETAILS_REQUEST:
+            return { ...state, loading: true };
+        case PRODUCT_DETAILS_SUCCESS:
+            return { loading: false, product: action.payload };
+        case PRODUCT_DETAILS_FAIL:
+            return { loading: false, error: action.payload };
+        default:
+            return state;
+    }
+};
+
