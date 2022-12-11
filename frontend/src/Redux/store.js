637d3bbb46b8d76d4dd24d05e51c38fed0a25ca3
Arraidh <ari.darmika@gmail.com>
2022-12-11 19:33:23 +0700

    Login Redux

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -3,21 +3,30 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { productDetailsReducer, productListReducer } from "./Reducers/ProductReducers.js";
 import { cartReducer } from "./Reducers/CartReducers.js";
+import { userLoginReducer } from "./Reducers/userReducer.js";
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
+    userLogin: userLoginReducer,
 });
 
 const cartItemsFromLocalStorage = localStorage.getItem("cartItem")
     ? JSON.parse(localStorage.getItem("cartItem"))
-    : []
+    : [];
+
+//login
+const userInfoFromLocalStorage = localStorage.getItem("userInfo")
+    ? JSON.parse(localStorage.getItem("userInfo"))
+    : null;
+
 
 const initialState = {
     cart: {
         cartItems: cartItemsFromLocalStorage
     },
+    userLogin: { userInfo: userInfoFromLocalStorage }
 };
 
 const middleware = [thunk]
