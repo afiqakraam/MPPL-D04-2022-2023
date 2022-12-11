f38dc1223dd7aa1060610a9a405bfbd77318b166
Arraidh <ari.darmika@gmail.com>
2022-12-11 15:36:58 +0700

    LOGIN (API)

--- server/Models/UserModel.js
+++ server/Models/UserModel.js
@@ -1,28 +1,35 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
-const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        require: true,
+const userSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            require: true,
+        },
+        email: {
+            type: String,
+            require: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            require: true,
+        },
+        isAdmin: {
+            type: Boolean,
+            require: true,
+            default: false,
+        },
     },
-    email: {
-        type: String,
-        require: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        require: true,
-    },
-    isAdmin: {
-        type: Boolean,
-        require: true,
-        default: false,
-    },
-},
     {
         timestamps: true
-    })
+    });
+
+// Login
+userSchema.methods.matchPassword = async function (enterPassword) {
+    return await bcrypt.compare(enterPassword, this.password);
+}
 
 const User = mongoose.model("User", userSchema);
 
