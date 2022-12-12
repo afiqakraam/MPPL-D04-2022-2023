2690f25232f060b0aa79747acd03a63bb43db184
Arraidh <ari.darmika@gmail.com>
2022-12-12 23:54:54 +0700

    Product Review API

--- server/Routes/ProductRoutes.js
+++ server/Routes/ProductRoutes.js
@@ -1,31 +1,69 @@
-import express from 'express';
+import express from "express";
 import asyncHandler from "express-async-handler";
-import Product from '../Models/ProductModel.js';
-
+import Product from "../Models/ProductModel.js";
+import protect from "../Middleware/AuthMiddleware.js";
 
 const productRoute = express.Router();
 
 //GET ALL PRODUCT
 productRoute.get(
-    "/",
-    asyncHandler(async (req, res) => {
-        const products = await Product.find({});
-        res.json(products);
-    }
-    ));
+  "/",
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
 
 // GET SINGLE PRODUCT
 productRoute.get(
-    "/:id",
-    asyncHandler(async (req, res) => {
-        const products = await Product.findById(req.params.id);
-        if (products) {
-            res.json(products);
-        } else {
-            res.status(404);
-            throw new Error("Product not Found");
-        }
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const products = await Product.findById(req.params.id);
+    if (products) {
+      res.json(products);
+    } else {
+      res.status(404);
+      throw new Error("Product not Found");
     }
-    ));
+  })
+);
+
+// PRODUCTT REVIEW
+productRoute.post(
+  "/:id/review",
+  protect,
+  asyncHandler(async (req, res) => {
+    const { rating, comment } = req.body;
+    const products = await Product.findById(req.params.id);
+
+    if (products) {
+      const alreadyReviewed = products.reviews.find(
+        (r) => r.user.toString() === req.user._id.toString()
+      );
+      if (alreadyReviewed) {
+        res.status(400);
+        throw new Error("Product already Reviewed");
+      }
+      const review = {
+        name: req.user.name,
+        rating: Number(rating),
+        comment,
+        user: req.user._id,
+      };
+
+      products.reviews.push(review);
+      productRoute.numReviews = products.reviews.length;
+      products.rating =
+        products.reviews.reduce((acc, item) => item.rating + acc, 0) /
+        products.reviews.length;
+
+      await products.save();
+      res.status(201).json({ message: "Reviewed Added" });
+    } else {
+      res.status(404);
+      throw new Error("Product not Found");
+    }
+  })
+);
 
 export default productRoute;
